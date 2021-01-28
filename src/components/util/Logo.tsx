1427215eea871e8fc98e172353728edcc8608466
asimhyff <asimhyff@users.noreply.github.com>
2021-01-28 12:03:25 +0500

    updated

--- src/components/util/Logo.tsx
+++ src/components/util/Logo.tsx
@@ -1,7 +1,25 @@
 export const Logo: React.FC = () => {
 	return (
-		<div className='logo'>
-			<h3>AH</h3>
-		</div>
+		<svg
+			width='65'
+			height='65'
+			viewBox='0 0 97 96'
+			fill='none'
+			className='logo-svg'
+			xmlns='http://www.w3.org/2000/svg'>
+			<rect
+				x='2'
+				y='2'
+				width='82'
+				height='76'
+				rx='10'
+				stroke='#5187D8'
+				stroke-width='4'
+			/>
+			<path
+				d='M35.3008 47.6875H25.9062L24.2656 53H16.9414L27.3711 24.5625H33.8164L44.3242 53H36.9609L35.3008 47.6875ZM27.5469 42.3945H33.6602L30.5938 32.5312L27.5469 42.3945ZM70.1836 53H63.3672V41.125H53.2695V53H46.4141V24.5625H53.2695V35.8516H63.3672V24.5625H70.1836V53Z'
+				fill='#C4C4C4'
+			/>
+		</svg>
 	)
 }
