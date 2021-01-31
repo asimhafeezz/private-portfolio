ed458a99a15a30c5517f04896403165de65757d1
asimhyff <asimhyff@users.noreply.github.com>
2021-01-31 22:27:03 +0500

    updated

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,12 +1,13 @@
 import { Logo } from '../icons/Logo'
-
 export const Appbar: React.FC = () => {
 	return (
 		<section className='appbar'>
 			<a href='https://asimhafeez.com'>
 				<Logo isLoading={false} />
 			</a>
-			<h2>Resume</h2>
+			<a href='/AsimHafeez.pdf' rel='noopener noreferrer' target='_blank'>
+				<h2>Resume</h2>
+			</a>
 		</section>
 	)
 }
