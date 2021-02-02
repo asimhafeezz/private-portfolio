b03fc8b996cf4ec5d759a192f84f4b8ad6798959
asimhyff <asimhyff@users.noreply.github.com>
2021-02-02 11:24:04 +0500

    text update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,7 +12,7 @@ export const projects: ProjectI[] = [
 	{
 		name: 'Portfolio',
 		desc:
-			'It the first version my personal website build in rectjs & designed using SCSS from scratch, without using any framework like Bootstrap or Material UI.  ',
+			'It the first version my personal website build in React.js & designed using SCSS from scratch, without using any UI framework like Bootstrap or Material UI.  ',
 		languages: 'Reactjs, Typescript, SCSS',
 		githubLink: '',
 		liveLink: 'https://asimhafeez.com',
